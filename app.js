c0716931b0af08606a0ffbe517a59178341a4e20
Mohamed129338 <81376424+Mohamed129338@users.noreply.github.com>
2021-06-28 16:55:05 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+//interducing all buttons
+var causes= document.getElementById("causes")
+var death_rate= document.getElementById("death-rate")
+var home = document.getElementById("home")
+ //making buttons work
+ home.onclick = () =>{
+     location.href="index.html?" 
+    }
+    causes.onclick = () =>{
+        location.href="causes.html?" 
+       }
+
+//liquid button???
+
